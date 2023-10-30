11b84b65b44314eac4b7b620759f2ff2e0c5ed71
Spandana <spandana1286@gmail.com>
2023-10-30 19:36:51 -0400

    Reducer

--- src/Labs/a4/ArrayStateVariable.js
+++ src/Labs/a4/ArrayStateVariable.js
@@ -1,12 +1,26 @@
-import React from 'react'
-
+import React, { useState } from "react";
 function ArrayStateVariable() {
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+  const addElement = () => {
+    setArray([...array, Math.floor(Math.random() * 100)]);
+  };
+  const deleteElement = (index) => {
+    setArray(array.filter((item, i) => i !== index));
+  };
   return (
     <div>
-        <h2> ArrayStateVariable</h2>
-        <button className='btn btn-sucess'>Add element</button>
+      <h2>Array State Variable</h2>
+      <button onClick={addElement}>Add Element</button>
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteElement(index)}>
+              Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
-
-export default ArrayStateVariable
+export default ArrayStateVariable;
--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -6,28 +6,34 @@ import Counter from "./Counter";
 import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
 import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import ReduxExamples from "./ReduxExamples";
+import TodoList from "./ReduxExamples/todos/TodoList";
 import StringStateVariables from "./StringStateVariable";
 
 function Assignment4() {
-  function sayHello(){
+  function sayHello() {
     alert("Hello");
   }
   return (
     <div>
       <h1>Assignment 4</h1>
-      <Add a={1} b={2}/>
-      <ClickEvent/>
-      <PassingDataOnEvent/>
-      <PassingFunctions theFunction={sayHello}/>
-      <EventObject/>
-      <Counter/>
-      <BooleanStateVariables/>
-      <StringStateVariables/>
-      <DateStateVariable/>
-      <ObjectStateVariable/>
-      <ArrayStateVariable/>
+      <Add a={1} b={2} />
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
+      <TodoList/>
     </div>
   );
 }
