b13fe3b83327931f239dc06e69fabd48c3fb7d17
Spandana <spandana1286@gmail.com>
2023-11-02 14:00:18 -0400

    Kanbas assignment4

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -3,12 +3,46 @@ import { useParams } from "react-router-dom";
 import db from "../../Database";
 import CollapsibleComponent from "./CollapsibleComponent";
 import "./index.css";
+import { useState } from "react";
 
 function ModuleList() {
   const { courseId } = useParams();
-  const modules = db.modules;
+  const [modules, setModules] = useState(db.modules);
+  const [module, setModule] = useState({
+    name: "New Module",
+    description: "New Description",
+    course: courseId,
+  });
+
   return (
     <ul className="list-group">
+      <li className="list-group-item">
+        <div className="mb-3 row">
+          <div className="col col-2">
+            <input
+              value={module.name}
+              onChange={(e) => setModule({ ...module, name: e.target.value })}
+              className="form-control"
+              placeholder="Module Name"
+            />
+          </div>
+          <div className="col">
+            <button className="btn btn-success">Add</button>
+          </div>
+        </div>
+        <div className="mb-3 col-3">
+          <textarea
+            value={module.description}
+            onChange={(e) =>
+              setModule({ ...module, description: e.target.value })
+            }
+            className="form-control"
+            placeholder="Module Description"
+            rows={4}
+          />
+        </div>
+      </li>
+
       {modules
         .filter((module) => module.course === courseId)
         .map((module, index) => (
