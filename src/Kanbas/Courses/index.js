38017643eae803f2da84b7731dce4660ce7316e0
Spandana <spandana1286@gmail.com>
2023-11-13 19:38:07 -0500

    added modules

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -15,11 +15,26 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/AssignmentEditor";
 import Grades from "./Grades";
 import "./index.css";
-function Courses({courses}) {
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Courses() {
   const { courseId } = useParams();
   const { pathname } = useLocation();
   const [empty, kanbas, _courses, id, screen] = pathname.split("/");
-  const course = courses.find((course) => course._id === courseId);
+  const URL = "http://localhost:4000/api/courses";
+  const [course, setCourse] = useState({});
+  const findCourseById = async (courseId) => {
+    const response = await axios.get(
+      `${URL}/${courseId}`
+    );
+    setCourse(response.data);
+  };
+
+  useEffect(() => {
+    findCourseById(courseId);
+  }, [courseId]);
+
   return (
     <div>
       <div className="course-heading">
