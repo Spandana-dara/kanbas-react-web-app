6cd139d8e914be52b5010974822eda95982acd25
Spandana <spandana1286@gmail.com>
2023-12-08 22:33:52 +0530

    changed landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div className="container">
-      <h1>Assignment</h1>
-      <div className="nav nav-pills">
-        <Link to="/Kanbas" className="nav-link">
-          Kanbas
-        </Link>
-        <Link to="/Labs" className="nav-link">
-          Labs
-        </Link>
-        <Link to="/project" className="nav-link">
-          Projects
-        </Link>
+      <div className="container">
+        <h2>CS 5610 : Web Development </h2>
+        <h4>Spandana Reddy Dara : 002625721</h4>
+        <nav className="nav nav-tabs mt-2">
+          <Link to="/Labs" className="nav-link">
+            Labs
+          </Link>
+          <Link to="/Kanbas" className="nav-link">
+            Kanbas
+          </Link>
+          <Link to="/project" className="nav-link">
+            Projects
+          </Link>
+        </nav>
       </div>
     </div>
   );
